e21e4f4a186e3577220270cc32ad894d00e459e3
vdfor <jiakun.li@qq.com>
2018-02-25 17:44:01 +0800

    update test api

--- src/api/test.ts
+++ src/api/test.ts
@@ -6,17 +6,9 @@ const router = new Router();
 router.prefix('/api/v0/test');
 
 router.get('/', async (ctx) => {
-  let a;
-  try {
-    a = fs.createReadStream('not exist');
-  } catch (error) {
-    console.log(111);
-    throw error;
-  }
-  ctx.body = a;
-  // ctx.body = {
-  //   result: 'test'
-  // };
+  ctx.body = {
+    result: 'test'
+  };
 });
 
 export default router;
