a0fa212e9df836fbadb64e4a7ef9db8f935773ec
vdfor <jiakun.li@qq.com>
2018-08-08 13:05:18 +0800

    更新react-sail接口

--- src/controller/react-sail/user.ts
+++ src/controller/react-sail/user.ts
@@ -4,7 +4,7 @@ class Controller {
 
   static async find(ctx: Koa.Context) {
     const { current, pageSize, name, sex } = ctx.query;
-    const [ dealCurrent, dealPageSize ] = [+current, +pageSize];
+    const [dealCurrent, dealPageSize] = [+current, +pageSize];
     const start = (dealCurrent - 1) * dealPageSize;
     const end = dealCurrent * dealPageSize;
     const resData = [];
@@ -18,50 +18,44 @@ class Controller {
       });
     }
     ctx.body = {
-      code: 0,
-      data: {
-        data: resData,
-        total: name || sex ? 200 : 1000
-      }
+      data: resData,
+      total: name || sex ? 200 : 1000
     };
   }
 
   static async findOne(ctx: Koa.Context) {
     const { id: i } = ctx.params;
     ctx.body = {
-      code: 0,
-      data: {
-        id: i,
-        code: Math.floor(Math.random() * 100),
-        name: `Name${i}`,
-        sex: i % 2 ? '男' : '女',
-        website: `https//${i}.example.com`,
-        qq: '1234567890' + i,
-        github: 'https://github.com/example' + i,
-        mobile: '1868888888' + i,
-        email: 'example' + i + '@example.com',
-        note: '备注信息' + i,
-        ceateAt: Date.now() - i * 1000 * 60 * 60 * 25
-      }
+      id: i,
+      code: Math.floor(Math.random() * 100),
+      name: `Name${i}`,
+      sex: i % 2 ? '男' : '女',
+      website: `https//${i}.example.com`,
+      qq: '1234567890' + i,
+      github: 'https://github.com/example' + i,
+      mobile: '1868888888' + i,
+      email: 'example' + i + '@example.com',
+      note: '备注信息' + i,
+      ceateAt: Date.now() - i * 1000 * 60 * 60 * 25
     };
   }
 
   static async update(ctx: Koa.Context) {
     const { id: i } = ctx.params;
-    ctx.body = {
-      code: i === 9 ? 500 : 0,
-      msg: i === 9 ? '更新失败' : '',
-      data: i === 9 ?  '更新失败' : '更新成功'
-    };
+    if (i === 9) {
+      ctx.throw(500, 'ID不匹配');
+      return;
+    }
+    ctx.body = { msg: 'success' };
   }
 
   static async drop(ctx: Koa.Context) {
     const { id: i } = ctx.params;
-    ctx.body = {
-      code: i === 3 ? 500 : 0,
-      msg: i === 3 ? '删除失败' : '',
-      data: i === 3 ?  '删除失败' : '删除成功'
-    };
+    if (i === 7) {
+      ctx.throw(500, 'ID不匹配');
+      return;
+    }
+    ctx.body = { msg: 'success' };
   }
 
 }
